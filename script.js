32f29c10ea1439c1ab3c56960f8db58b5d0dd084
Mikee Ojastro <mikeeojastro@gmail.com>
2024-09-23 20:07:14 +0800

    September 23, 2024

--- script.js
+++ script.js
@@ -26,3 +26,41 @@ document.querySelector(".third").style.backgroundColor = "purple";
 
 console.log(document.querySelectorAll(".sample"));
 document.querySelectorAll(".sample")[2].style.backgroundColor = "green";
+
+let about = document.getElementById("about");
+// creating a new div
+let newDiv = document.createElement("div");
+// creating a new button
+let newButton = document.createElement("button");
+newButton.textContent = "Alert!";
+newButton.style.color = "black";
+newButton.style.border = "1px";
+newButton.style.margin = "10px";
+newButton.style.padding = "15px 30px";
+newButton.style.borderRadius = "15px";
+newButton.style.backgroundColor = "orange";
+
+newDiv.appendChild(newButton);
+about.appendChild(newDiv);
+
+function sayHey(){
+    alert("Hey!");
+}
+
+// first parameter - action enclosed in quotation marks
+// second parameter - declared function
+newButton.addEventListener('click', sayHey);
+
+function incrementOne() {
+    // This is adding number to string
+    // counter.textContent = counter.textContent + 1;
+    // counter.textContent += 1;
+    let parseToNumber = parseInt(counter.textContent);
+    // parseToNumber = parseToNumber + 1;
+    parseToNumber += 1;
+    counter.textContent = parseToNumber;
+}
+
+let counter = document.getElementById("counter");
+let increment = document.getElementById("increment");
+increment.addEventListener("click", incrementOne);
